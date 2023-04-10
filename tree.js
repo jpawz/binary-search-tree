8ee527c95d2496d29114587b6b12bfbbd06237c4
Paweł J <jpawz@outlook.com>
2023-04-10 20:49:50 +0200

    Add buildTree function

--- tree.js
+++ tree.js
@@ -1,7 +1,18 @@
+import { Node } from "./node";
+
 export class Tree {
   constructor(arr) {
     this.root = this.buildTree(arr);
   }
 
-  buildTree(arr) {}
+  buildTree(arr) {
+    const values = [...new Set(arr)].sort();
+    const middleIndex = Math.floor(values.length / 2);
+    const rootNode = new Node(arr[middleIndex], null, null);
+
+    rootNode.leftChild = this.buildTree(arr.slice(0, middleIndex));
+    rootNode.rightChild = this.buildTree(arr.slice(middleIndex + 1));
+
+    return rootNode;
+  }
 }
